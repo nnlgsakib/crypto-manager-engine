5b5e03f1cd50a3452e4b4f9acf3fc6df4224c07f
nnlgsakib <nlgarts@gmail.com>
2025-06-20 16:41:12 +0600

    updated the port

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@
 import app from './app';
 import { logger } from './utils/logger';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8484;
 
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
